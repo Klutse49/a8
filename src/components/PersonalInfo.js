b5bb0854da980af9459d1271c52f15029897e22e
Klutse49 <144399156+Klutse49@users.noreply.github.com>
2024-03-11 19:53:19 -0400

    Personal info edited

---
+++ src/components/PersonalInfo.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import styles from './PersonalInfo.module.css'; 
+
+const PersonalInfo = ({ personalInfo }) => {
+  return (
+    <section className={styles.personalInfo}>
+      <h2>Personal Information</h2>
+      <img src={personalInfo.profilePicture} alt="Profile Picture" className={styles.profilePicture} />
+      <p>Date of Birth: {personalInfo.dob}</p>
+      <p>Hometown: {personalInfo.hometown}</p>
+      <h3>Hobbies and Interests</h3>
+      <ul>
+        {personalInfo.hobbies.map((hobby, index) => (
+          <li key={index}>{hobby}</li>
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+export default PersonalInfo;
