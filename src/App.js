f388d46c5d6295407440ac5e4f749897f33b206b
Klutse49 <144399156+Klutse49@users.noreply.github.com>
2024-03-11 22:27:18 -0400

    edit app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+import PersonalInfo from './components/PersonalInfo';
+import Education from './components/Education';
+import Skills from './components/Skills';
+import RealTimeData from './components/RealTimeData';
+import Contact from './components/Contact';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PersonalInfo />
+      <Education />
+      <Skills />
+      <RealTimeData />
+      <Contact />
     </div>
   );
 }
