52a0dc04b5edf11ccda539b697544cd6c0d348c2
Klutse49 <144399156+Klutse49@users.noreply.github.com>
2024-03-12 00:44:33 -0400

    minimize app.js

--- src/App.js
+++ src/App.js
@@ -1,52 +1,28 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header';
-import Footer from './components/Footer';
+import Header from './components/Header'; 
+import Footer from './components/Footer'; 
 import PersonalInfo from './components/PersonalInfo';
 import Education from './components/Education';
 import Skills from './components/Skills';
-import Contact from './components/Contact';
 import WeatherAndQuote from './components/WeatherAndQuote';
+import Contact from './components/Contact';
 import { personalInfo, education, skills } from './data/data';
-
 
 function App() {
   return (
     <div className="App">
-      
-      <header className="App-header">
-        <h1>Cyril Klutse</h1>
-        <nav>
-          <ul>
-            <li><a href="#hero">Home</a></li>
-            <li><a href="#personal-info">Personal Info</a></li>
-            <li><a href="#education">Education</a></li>
-            <li><a href="#skills">Skills</a></li>
-            <li><a href="#weatherandquote">Real Time Data</a></li>
-            <li><a href="#contact">Contact</a></li>
-          </ul>
-        </nav>
-      </header>
+      <Header /> 
       
       <main>
         <PersonalInfo personalInfo={personalInfo} />
         <Education education={education} />
         <Skills skills={skills} />
-        <Contact />
         <WeatherAndQuote />
+        <Contact />
       </main>
       
-      <footer>
-        <div>
-          <h2>Contact Information</h2>
-          <p>Email: cyrilklutse@gmail.com</p>
-          <p>Phone: +1(908)-963-3628</p>
-          <div className="social-links">
-            <a href="https://twitter.com/cyrilklutse">Twitter</a>
-            <a href="https://www.linkedin.com/in/cyril-klutse-991b76113/">LinkedIn</a>
-          </div>
-        </div>
-      </footer>
+      <Footer /> 
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import styles from '../styles/Header.module.css'; 
 
-const Header = () => (
-  <header className={styles.header}>
-    <h1>Cyril Klutse</h1>
-    <nav>
-      <ul className={styles.navUl}>
-        <li className={styles.navItem}><a href="#hero" className={styles.navLink}>Home</a></li>
-        <li className={styles.navItem}><a href="#personal-info" className={styles.navLink}>Personal Info</a></li>
-        <li className={styles.navItem}><a href="#education" className={styles.navLink}>Education</a></li>
-        <li className={styles.navItem}><a href="#skills" className={styles.navLink}>Skills</a></li>
-        <li className={styles.navItem}><a href="#real-time-data" className={styles.navLink}>Real Time Data</a></li>
-        <li className={styles.navItem}><a href="#contact" className={styles.navLink}>Contact</a></li>
-      </ul>
-    </nav>
-  </header>
-);
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <h1>Cyril Klutse</h1>
+      <nav>
+        <ul className={styles.navList}>
+          <li className={styles.navItem}><a href="#hero" className={styles.navLink}>Home</a></li>
+          <li className={styles.navItem}><a href="#personal-info" className={styles.navLink}>Personal Info</a></li>
+          <li className={styles.navItem}><a href="#education" className={styles.navLink}>Education</a></li>
+          <li className={styles.navItem}><a href="#skills" className={styles.navLink}>Skills</a></li>
+          <li className={styles.navItem}><a href="#weatherandquote" className={styles.navLink}>Weather Data</a></li>
+          <li className={styles.navItem}><a href="#contact" className={styles.navLink}>Contact</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
 export default Header;
