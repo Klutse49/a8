3e2651c86c310191963f39072d97934501b2118c
Klutse49 <144399156+Klutse49@users.noreply.github.com>
2024-03-11 22:39:21 -0400

    apps.js fixed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import './App.css';
+import './App.css'; 
+
 
 import PersonalInfo from './components/PersonalInfo';
 import Education from './components/Education';
@@ -7,16 +8,42 @@ import Skills from './components/Skills';
 import RealTimeData from './components/RealTimeData';
 import Contact from './components/Contact';
 
-
-
 function App() {
   return (
     <div className="App">
-      <PersonalInfo />
-      <Education />
-      <Skills />
-      <RealTimeData />
-      <Contact />
+      <header className="App-header">
+        <h1>Cyril Klutse</h1>
+        <nav>
+          <ul>
+            <li><a href="#hero">Home</a></li>
+            <li><a href="#personal-info">Personal Info</a></li>
+            <li><a href="#education">Education</a></li>
+            <li><a href="#skills">Skills</a></li>
+            <li><a href="#real-time-data">Real Time Data</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+        </nav>
+      </header>
+      
+      <main>
+        <PersonalInfo />
+        <Education />
+        <Skills />
+        <RealTimeData />
+        <Contact />
+      </main>
+      
+      <footer>
+        <div>
+          <h2>Contact Information</h2>
+          <p>Email: cyrilklutse@gmail.com</p>
+          <p>Phone: +1(908)-963-3628</p>
+          <div className="social-links">
+            <a href="https://twitter.com/cyrilklutse">Twitter</a>
+            <a href="https://www.linkedin.com/in/cyril-klutse-991b76113/">LinkedIn</a>
+          </div>
+        </div>
+      </footer>
     </div>
   );
 }
